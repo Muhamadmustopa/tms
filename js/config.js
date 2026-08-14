9cea49dbb94905c19edb61ef21b5aa648b554d34
Muhamadmustopa <82715396+Muhamadmustopa@users.noreply.github.com>
2026-08-14 08:32:37 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
-const CONFIG={googleScriptUrl:"https://script.google.com/macros/s/AKfycbxhgHyJeH3UgkEgRLsPRcB9dMa_nvLgZcf3H3ghe2EciLki1TJq8-lLJP5gTzULuhSPoQ/exec",passingGrade:75};
+const CONFIG = {
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycbxhGHyJeH3UgkEgRLsPRcB9dMa_nvLgZcf3H3ghe2EciLki1TJq8-lLJP5gTzULuhSPoQ/exec",
+
+  passingGrade: 75,
+
+  trainingName: "Learning and Development",
+
+  // Peserta yang emailnya sudah ada di Google Sheet
+  // tidak boleh mengikuti training kembali.
+  preventDuplicate: true
+};
