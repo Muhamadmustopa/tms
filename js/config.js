9fe7051081b7d7e4780baf5dbd24210da1fa5d65
Muhamadmustopa <82715396+Muhamadmustopa@users.noreply.github.com>
2026-08-14 16:54:07 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  googleScriptUrl: "https://script.google.com/macros/s/AKfycbxhGHyJeH3UgkEgRLsPRcB9dMa_nvLgZcf3H3ghe2EciLki1TJq8-lLJP5gTzULuhSPoQ/exec",
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycbxhgHyJeH3UgkEgRLsPRcB9dMa_nvLgZcf3H3ghe2EciLki1TJq8-lLJP5gTzULuhSPoQ/exec",
 
   passingGrade: 75,
 
